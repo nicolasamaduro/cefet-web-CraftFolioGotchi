468d2e80284c43ecfddc786439232c390b6b8b95
Nícolas Maduro <nicolasamaduro@gmail.com>
2018-06-25 19:51:10 -0300

    VAI HEROKU

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use(express.static(path.join(__dirname, '..', 'static')))
 app.use(express.static(path.join(__dirname, '..', 'node_modules')))
 app.use(bodyParser.json());
 
-controller.set(app);
+//controller.set(app);
 
 const port =process.env.PORT || 3000;
 
